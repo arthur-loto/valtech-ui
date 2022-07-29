eb9890cca7cd8b1e81c2a64671b7e5392173f626
Arthur Rigaud <arthur.rigaud@loto-quebec.com>
2022-07-29 08:52:55 -0400

    Premier commit

---
+++ src/components/VCard/VCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="classes">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import './VCard.scss'
+import { computed } from 'vue'
+
+interface Props {
+  primary: boolean
+}
+
+const props = defineProps<Props>()
+
+const classes = computed(() => {
+  return {
+    'v-card': true,
+  }
+})
+</script>
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import VButton from './VButton/VButton.vue'
+import VCard from './VCard/VCard.vue'
+
+export {
+  VButton,
+  VCard,
+}
